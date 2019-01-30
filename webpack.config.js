1bfd7839e97b4450f1d61cd43bb789259b643aa1
Marc Barbeau <mabarbeau1@gmail.com>
2019-01-29 17:28:49 -0800

    Add material icons

--- webpack.config.js
+++ webpack.config.js
@@ -44,7 +44,7 @@ module.exports = {
         ]
       },
       {
-        test: /\.(png|svg|jpg|gif)$/,
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
         use: [
           'file-loader'
         ]
