ac706e61d46a4cf43a5c082aa45dd2a5a7df91fc
Marc Barbeau <mabarbeau1@gmail.com>
2019-04-07 09:52:06 -0700

    Rename base button class: '.mdc-button' -> '.btn'

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import './styles/main.scss';
 
 import {MDCRipple} from '@material/ripple';
 
-const buttonRipple = new MDCRipple(document.querySelector('.mdc-button'));
+const buttonRipple = new MDCRipple(document.querySelector('.btn'));
